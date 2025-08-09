b09983aa1bb0e60160d5ac7c67adf1c738e86e62
AxlRamses <ramses.coto1@gmail.com>
2025-08-08 19:58:24 -0600

    Agregar sistema de gestion de tareas

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import com.gestor.tareas.GestorTareas;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        GestorTareas gestor = new GestorTareas();
+        int opcion;
+        do {
+
+
+        }
+
+
+    }
+}
---
+++ src/com/gestor/tareas/GestorTareas.java
@@ -0,0 +1,4 @@
+package com.gestor.tareas;
+
+public class GestorTareas {
+}
